713f68cac4cb66ca0f55634eade3963fe90daa9a
Roy Tang <me@roytang.me>
2017-03-02 23:06:08 +0800

    users wip

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,29 @@
 var express = require('express')
 var router = express.Router()
 var getLinkedInProfile = require('../app/common/linkedInUtils').getLinkedInProfile
+var User = require('../models/User')
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  if (!req.session.passport.user.accessToken) {
-    res.json({error: 'accessToken not found'})
-    return
-  }
+router.put('/update', function (req, res, next) {
+  // if (!req.session.passport.user.accessToken) {
+  //   res.json({error: 'accessToken not found'})
+  //   return
+  // }
 
-  getLinkedInProfile(req.session.passport.user.accessToken)
-  res.json(req.session)
+  let user = new User({
+    username: req.body.username,
+    level: 'user',
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    emailAddress: req.body.emailAddress
+  })
+
+  user.save({upsert: true})
+  .then((result) => {
+    console.log(result)
+    res.json(result)
+  })
+  .catch(err => { console.error(err); res.json(err) })
 })
 
 module.exports = router
